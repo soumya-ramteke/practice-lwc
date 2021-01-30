f6f3bd08c1091593934c7b1fa7cd7748ce66ef16
soumya-ramteke <soumya.r@cgi.com>
2021-01-30 20:08:22 +0530

    parent child basics

---
+++ force-app/main/default/lwc/compositionBasics/compositionBasics.js
@@ -0,0 +1,10 @@
+import { LightningElement } from "lwc";
+
+export default class CompositionBasics extends LightningElement {
+  contact = {
+    Name: "Amy Taylor",
+    Title: "VP of Engineering",
+    Picture:
+      "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/amy_taylor.jpg"
+  };
+}
