092ab063f4290f65f41d5a9d08aa60f8e15b2b01
soumya-ramteke <soumya.r@cgi.com>
2021-01-30 20:38:58 +0530

    send event from child to owner

--- force-app/main/default/lwc/compositionBasics/compositionBasics.js
+++ force-app/main/default/lwc/compositionBasics/compositionBasics.js
@@ -1,10 +1,29 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 
 export default class CompositionBasics extends LightningElement {
-  contact = {
+  @track contact = {
     Name: "Amy Taylor",
-    Title: "VP of Engineering",
-    Picture:
-      "https://s3-us-west-1.amazonaws.com/sfdc-demo/people/amy_taylor.jpg"
+    Title: "VP of Engineering"
   };
+  handleUpdateContact() {
+    this.contact = {
+      Name: "Michael Jones",
+      Title: "VP of Sales"
+    };
+    console.log("handleUpdateContact called, contact:" + this.contact);
+    // try just changing the just the title of the contact
+    // this.contact.Title = "ASE";
+    // console.log(this.contact.Title);
+    // console.log(this.contact);
+    // the changes doesn't reflect in page,  contact title is changed when we print in console
+    // if we define it as @track contact then it listens to changes and updates the name
+  }
+  handleUpdateContactName() {
+    // we don't mark contact as @track, the value of name would be changed but won't be reflected
+    this.contact.Name = "Soumya Ramteke";
+    console.log(
+      "handleUpdateContact called, contact.Name:" + this.contact.Name
+    );
+    console.log("handleUpdateContact called, contact:" + this.contact);
+  }
 }
--- force-app/main/default/lwc/contactTile/contactTile.js
+++ force-app/main/default/lwc/contactTile/contactTile.js
@@ -2,4 +2,15 @@ import { LightningElement, api } from "lwc";
 
 export default class ContactTile extends LightningElement {
   @api contact1;
+  updateContact() {
+    // firing an event upon click event of the button
+    this.dispatchEvent(new CustomEvent("updatecontact"));
+    // now the parent event would be listening for this event
+    // where??
+    // in <c-contact-tile onupdatecontact={handleUpdate}> in compositionBasics.html
+    // listening to the event where we have the child component markup
+  }
+  updateContactName() {
+    this.dispatchEvent(new CustomEvent("updatecontactname"));
+  }
 }
